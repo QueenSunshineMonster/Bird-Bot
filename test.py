cd4d539da6501c2bae8446052bc6141baff3f167
QueenSunshineMonster <65317536+QueenSunshineMonster@users.noreply.github.com>
2020-06-26 20:42:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-print("Hello, world")
-print("this is awesome")
+/*
+  Discord WebHook Example for Mkr Wifi 1010
+*/
+
+#include "discord.h"
+
+void setup() {
+  Serial.begin(9600);
+  connect_to_wifi();
+  discord_send("Hello World");
+}
+
+void loop() {
+}
